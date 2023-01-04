20efe3642292767b4bae268b0d030b3f54b6208a
Limonedi <holuloldoce@gmail.com>
2023-01-04 10:36:40 -0500

    listo

--- src/app/home/body/body.component.ts
+++ src/app/home/body/body.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+interface Task {
+  name: string;
+  description: string;
+}
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
   styleUrls: ['./body.component.css']
 })
 export class BodyComponent implements OnInit {
+  taskName = '';
+  taskDescription = '';
+  tasks: Task[] = [];
 
+  addTask() {
+    this.tasks.push({
+      name: this.taskName,
+      description: this.taskDescription
+    });
+    this.taskName = '';
+    this.taskDescription = '';
+  }
   constructor() { }
 
   ngOnInit(): void {
   }
 
-}
+} 
